aabfbe5bf64cf3b088bba94d533accaa360bc74a
jiyongkwack <rhkrwldyd@gmail.com>
2020-11-18 01:20:51 +0900

    db셋팅 재설정

--- src/main/java/com/manage/alhl/dao/UserDAO.java
+++ src/main/java/com/manage/alhl/dao/UserDAO.java
@@ -4,56 +4,75 @@ import java.util.List;
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import com.manage.alhl.dto.UserDTO;
 import com.manage.alhl.util.SHA256;
 
-
+@Repository
 public class UserDAO {
+	// 컨테이너가 객체를 자동으로 생성 Autowired
+	@Autowired
+	public SqlSessionTemplate mybatis;
 
-	/*
-	 * // 컨테이너가 객체를 자동으로 생성 Autowired
-	 * 
-	 * @Autowired public SqlSessionTemplate mybatis;
-	 * 
-	 * @Override public UserDTO userSelectOne(UserDTO dto) { // TODO Auto-generated
-	 * method stub dto.setUserPassword(SHA256.getSHA256(dto.getUserPassword()));
-	 * return mybatis.selectOne("userMapper.userSelectOne", dto); }
-	 * 
-	 * @Override public int userInsert(UserDTO dto) { // TODO Auto-generated method
-	 * stub dto.setUserPassword(SHA256.getSHA256(dto.getUserPassword())); // 패스워드
-	 * 해쉬화 java.sql.Timestamp creatTime = new java.sql.Timestamp(new
-	 * java.util.Date().getTime()); //현재시간을 구해서 넣음 dto.setCreatTime(creatTime); //
-	 * 현재시간 담음 dto.setUserEmailHash(SHA256.getSHA256(dto.getUserEmail())); // 이메일
-	 * 해쉬화를 통한 인증코드 dto.setUserEmailCertified(0);//기본 0으로 설정 return
-	 * mybatis.insert("userMapper.userInsert", dto); }
-	 * 
-	 * @Override public List<UserDTO> userSelect() { // TODO Auto-generated method
-	 * stub return null; }
-	 * 
-	 * 
-	 * @Override public int userIdCheck(UserDTO dto) { int result = 1; UserDTO
-	 * resDTO = mybatis.selectOne("userMapper.userSelectOne", dto);
-	 * 
-	 * try { resDTO.getUserId(); }catch(NullPointerException e) { //널값이면 중복아님 result
-	 * = 0; } System.out.println("결과:"+result); return result; }
-	 * 
-	 * 
-	 * @Override public int userUpdate_Cash(UserDTO dto) { if(dto.getCash() < 0) {
-	 * System.out.println("초과된 값을 결제할 수 없습니다."); return 0; }else { return
-	 * mybatis.update("userMapper.userUpdate_Cash",dto); } }
-	 * 
-	 * @Override public int userUpdate(UserDTO dto) {
-	 * dto.setUserPassword(SHA256.getSHA256(dto.getUserPassword())); return
-	 * mybatis.update("userMapper.userModify",dto); }
-	 * 
-	 * @Override public int userDelete(UserDTO dto) { return
-	 * mybatis.delete("userMapper.userDelete", dto); }
-	 * 
-	 * @Override public int userEmailCheck(UserDTO dto) { UserDTO udto =
-	 * mybatis.selectOne("userMapper.userSelectOne", dto);
-	 * if(udto.getUserEmailCertified()==1) { return 2; }else
-	 * if(udto.getUserEmailHash().equals(dto.getUserEmailHash())) { return
-	 * mybatis.delete("userMapper.userEmailCheck",dto); }else { return 0; } }
-	 */
+	public UserDTO userSelectOne(UserDTO dto) {
+		// TODO Auto-generated method stub
+		dto.setUserPassword(SHA256.getSHA256(dto.getUserPassword()));
+		return mybatis.selectOne("userMapper.userSelectOne", dto);
+	}
+	public int userInsert(UserDTO dto) {
+		// TODO Auto-generated method stub
+		dto.setUserPassword(SHA256.getSHA256(dto.getUserPassword())); // 패스워드 해쉬화
+		java.sql.Timestamp creatTime = new java.sql.Timestamp(new java.util.Date().getTime()); // 현재시간을 구해서 넣음
+		dto.setCreatTime(creatTime); // 현재시간 담음
+		dto.setUserEmailHash(SHA256.getSHA256(dto.getUserEmail())); // 이메일 해쉬화를 통한 인증코드
+		dto.setUserEmailCertified(0);// 기본 0으로 설정
+		return mybatis.insert("userMapper.userInsert", dto);
+	}
+
+	public List<UserDTO> userSelect() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int userIdCheck(UserDTO dto) {
+		int result = 1;
+		UserDTO resDTO = mybatis.selectOne("userMapper.userSelectOne", dto);
+
+		try {
+			resDTO.getUserId();
+		} catch (NullPointerException e) { // 널값이면 중복아님
+			result = 0;
+		}
+		System.out.println("결과:" + result);
+		return result;
+	}
+	public int userUpdate_Cash(UserDTO dto) {
+		if (dto.getCash() < 0) {
+			System.out.println("초과된 값을 결제할 수 없습니다.");
+			return 0;
+		} else {
+			return mybatis.update("userMapper.userUpdate_Cash", dto);
+		}
+	}
+
+	public int userUpdate(UserDTO dto) {
+		dto.setUserPassword(SHA256.getSHA256(dto.getUserPassword()));
+		return mybatis.update("userMapper.userModify", dto);
+	}
+
+	public int userDelete(UserDTO dto) {
+		return mybatis.delete("userMapper.userDelete", dto);
+	}
+
+	public int userEmailCheck(UserDTO dto) {
+		UserDTO udto = mybatis.selectOne("userMapper.userSelectOne", dto);
+		if (udto.getUserEmailCertified() == 1) {
+			return 2;
+		} else if (udto.getUserEmailHash().equals(dto.getUserEmailHash())) {
+			return mybatis.delete("userMapper.userEmailCheck", dto);
+		} else {
+			return 0;
+		}
+	}
 }
---
+++ src/main/java/com/manage/alhl/service/UserService.java
@@ -0,0 +1,45 @@
+package com.manage.alhl.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Service;
+
+import com.manage.alhl.dao.UserDAO;
+import com.manage.alhl.dto.UserDTO;
+
+
+//[DB연결 사용법] 6. 서비스 클래스 작성
+@Service("UserService")
+public class UserService{
+	// 인터페이스로 생성해야함
+	@Autowired
+	public UserDAO dao;
+	
+	public List<UserDTO> userSelect() {
+		return dao.userSelect();
+	}
+	public UserDTO userSelectOne(UserDTO dto) {
+		return dao.userSelectOne(dto);
+	}
+	public int userInsert(UserDTO dto) {
+		return dao.userInsert(dto);
+	}
+	
+	public int userIdCheck(UserDTO dto){
+		return dao.userIdCheck(dto);
+	}
+	public int userUpdate_Cash(UserDTO dto) {
+		return dao.userUpdate_Cash(dto);
+	}
+	public int userUpdate(UserDTO dto) {
+		return dao.userUpdate(dto);
+	}
+	public int userDelete(UserDTO dto) {
+		return dao.userDelete(dto);
+	}
+	public int userEmailCheck(UserDTO dto) {
+		return dao.userEmailCheck(dto);
+	}
+}
